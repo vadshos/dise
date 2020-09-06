134405103d34f4cc4ae80c068a5da30e75803806
vadshos <66492574+vadshos@users.noreply.github.com>
2020-09-06 18:33:06 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,39 +3,42 @@
 #include"windows.h"
 
 using namespace std;
+int win = 0, newVictorins = 0, choiseVictorins = 0, choiseVariants = 0, victorines = 0, questiones = 0, variants = 0;
+string* victorine = new string [victorines];
+string* questions = new string[questiones];
+string* variantes = new string[variants];
+string* trueVariantes = new string[questiones];
+int* indexForOutput = new int  [victorines];
+int* indexForOutput2 = new int[victorines];
 
-string victorine[100][10][6] = { { {"Ancient Greece. Myths","How did Hera thank Cancer, who helped the Lerneian Hydra in the duel with Hercules?","She gave him a river","Made him a constellation","Made him immortal","Made him a constellation"},{"Ancient Greece. Myths","This king ordered the daughters to kill their brides, for which they were doomed by the gods to carry water in a bottomless amphora without ceasing in the gloomy kingdom of Hades.","Oedipus","Danai","Egypt","Danai"},{"Ancient Greece. Myths","Who sympathized with the suffering of Prometheus, chained to the Caucasus Mountains?","Moiri","Oceanids","Eat","Oceanids"},{"Ancient Greece. Myths","Daughter of King Colchis Eeta, who helped Jason steal the golden fleece.","Hestia","Iris","Medea","Medea"},{"Ancient Greece. Myths","What did Odysseus call himself Cyclops Polyphemus?","No one","Nothing","Someone","No one"},{"Ancient Greece. Myths","6. What ordered Hercules to get the Amazon insignificant Eurystheus?","3 pieces of Melanippus","Horse of Antioch","Hippolyta's belt","Hippolyta's belt"},{"Ancient Greece. Myths","This impudent son of 3evs not only stole ragweed and nectar from the gods, but also decided to treat them to an unheard of dish - baked from his own son Pelops. For this he was punished with eternal torment from hunger, thirst and fear.","Antey","Sisyphus","Tantalum","Tantalum"},{"Ancient Greece. Myths","Rebellious and proud Theban queen, daughter of Tantalus, who offended the goddess Leto. She was punished by her children Apollo and Artemis - they killed all her children.","Given","Pasiphae","Niobe","Niobe"},{"Ancient Greece. Myths","The unsurpassed master weaver, who challenged Athena Pallas herself, despised the gods. Athena turned her into a spider.","Arachna","Ariadne","Medea","Arachna"},{"Ancient Greece. Myths","The son of Danai, who defeated Medusa the Gorgon and saved Andromeda, the daughter of King Cepheus and Cassiopeia, from imminent death.","Odysseus","Perseus","Phineas","Perseus"} } };
-int win = 0, newVictorins = 0, choiseVictorins = 0, choiseVariants = 0, victorines = 0;
+
 void SetColor(int text, int bg) {
 	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hStdOut, (WORD)((bg << 4) | text));
 }
 void output() {
 	SetColor(15, 0);
-	for (int i = 0; i < 10; i++) {
-		for (int j = 1; j < 5; j++) {
-			cout << victorine[choiseVictorins][i][j] << endl;
+	for (int i = indexForOutput[choiseVictorins - 1]; i < indexForOutput[choiseVictorins]; i++) {
+		for (int j = indexForOutput[choiseVictorins - 1]; j < indexForOutput[choiseVictorins]; j++) {
+			variantes[j];
 		}
 		cout << "Enter number variants-> ";
 		cin >> choiseVariants;
 		system("cls");
 		SetColor(15, 0);
-		for (int j = 1; j < 5; j++) {
-			if (victorine[choiseVictorins][i][j] == victorine[choiseVictorins][i][5]) {
+		for (int j = indexForOutput[choiseVictorins - 1]; j < indexForOutput[choiseVictorins]; j++) {
+			if (variantes[choiseVictorins - 1] == trueVariantes[choiseVictorins - 1]) {
 				SetColor(2, 0);
-				cout << victorine[choiseVictorins][i][j] << endl;
+				variantes[j];
 			}
-			else if (victorine[choiseVictorins][i][j] == victorine[choiseVictorins][i][1]) {
-				SetColor(15, 0);
-				cout << victorine[choiseVictorins][i][j] << endl;
-			}
+			
 			else {
 				SetColor(4, 0);
-				cout << victorine[choiseVictorins][i][j] << endl;
+				variantes[j];
 			}
 		}
 		SetColor(15, 0);
-		if (victorine[choiseVictorins][i][choiseVariants++] == victorine[choiseVictorins][i][5]) {
+		if (variantes[choiseVariants-1] == trueVariantes[i]) {
 			win++;
 		}
 
@@ -43,28 +46,69 @@ void output() {
 	cout << "Correct answers:" << win << " out of 10" << endl;
 }
 void addNewVictirines() {
-	victorines+1;
-	cout << "Enter name your victorines" << endl;
-	getline(cin, victorine[victorines][0][0]);
-	getline(cin, victorine[victorines][0][0]);
-	for (int i = 0; i < 10; i++) {
-		 victorine[victorines][i][0] = victorine[victorines][0][0];;
-	}
-	for (int i = 0; i < 10; i++) {
-		cout << "Enter question";
-		cin >> victorine[victorines][i][1];
-		for (int j = 2; j < 5; j++) {
-			cout << "Enter variants ";
-			cin>> victorine[victorines][i][j];
-			int  trueVar =0;
-			cout << "This varians true ";
-			cin >> trueVar;
-			system("cls");
-			if (trueVar == 1) {
-				victorine[victorines][i][5] = victorine[victorines][i][j];
+	int question = 0;
+	int variantes2 = 0;
+	int writeStartNumber = 0;
+	victorines + 1;
+	cout << "How many question you want -> ";
+	cin >> question;
+	cout << "How many variants you want -> ";
+	cin >> variantes2;
+	questiones += question;
+	variants += variantes2;
+
+	system("cls");
+	indexForOutput[victorines - 1] = variants;
+	indexForOutput2[victorines - 1] = question;
+	string* tempVictorines = new string[1];
+	string* tempQuestion = new string[question];
+	string* tempVariantes = new string[variants*question];
+	string* tempTrueVariantes = new string[question];
+	for (int i = 0; i < victorines+1;i++) {
+		cout << "Enter name your victorines" << endl;
+		getline(cin, tempVictorines[victorines]);
+		for (int j = questiones; j <( question + questiones+1); j++)
+		{
+			cout << "enter question" << endl;
+			getline(cin, tempQuestion[j]);
+			
+			for (int g = writeStartNumber; g <( writeStartNumber + variantes2); g++)
+			{
+				bool trueVariants = false;
+				cout << "enter variants  " << endl;
+				getline(cin, tempVariantes[g]);
+				if (trueVariants == false) {
+					cout << "this variants is true";
+					string ach;
+					cin >> ach;
+					if (ach == "yes") {
+						tempTrueVariantes[j] = tempVariantes[g];
+						trueVariants = true;
+					}
+					else {
+						continue;
+					}
+				}
+				
 			}
+			writeStartNumber += variantes2;
 		}
 	}
+	victorine[victorines] = tempVictorines[0];
+	for (int i = (questiones - question); i < (questiones + question); i++) {
+		questions[i] = tempQuestion[i - questiones];
+		trueVariantes[i] = tempTrueVariantes[i];
+	}
+	for (int i = (variants - variantes2); i < (variants + variantes2); i++) {
+		variantes[i] = tempVariantes[i - variants];
+	}
+	
+
+	delete[]victorine;
+	delete[]questions;
+	delete[]variantes;
+	delete[]trueVariantes;
+
 }
 int main() {
 	int acshion = 0;
@@ -78,8 +122,8 @@ int main() {
 			addNewVictirines();
 		}
 		else if (acshion == 2) {
-			for (int i = 0; i < victorines+1; i++) {
-				cout << victorine[i][0][0] << endl;
+			for (int i = 0; i < victorines + 1; i++) {
+				cout << victorine[i] << endl;
 				cout << "Choise victorines enter number ->";
 				cin >> choiseVictorins;
 				choiseVictorins--;
@@ -87,7 +131,6 @@ int main() {
 				output();
 			}
 		}
-		
+
 	} while (acshion != 3);
 }
-
