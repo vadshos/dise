aee0a258a079e37e318d571129a32c5f3f460fc4
vadshos <66492574+vadshos@users.noreply.github.com>
2020-09-07 22:47:25 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,12 +4,15 @@
 
 using namespace std;
 int win = 0, newVictorins = 0, choiseVictorins = 0, choiseVariants = 0, victorines = 0, questiones = 0, variants = 0;
-string* victorine = new string [victorines];
+string* victorine = new string[victorines];
 string* questions = new string[questiones];
 string* variantes = new string[variants];
-string* trueVariantes = new string[questiones];
-int* indexForOutput = new int  [victorines];
+int* trueVariantes = new int[questiones];
+int* indexForOutput = new int[victorines];
 int* indexForOutput2 = new int[victorines];
+int* startQuestion = new int[victorines];
+int* startVariantes = new int[victorines];
+
 
 
 void SetColor(int text, int bg) {
@@ -18,96 +21,110 @@ void SetColor(int text, int bg) {
 }
 void output() {
 	SetColor(15, 0);
-	for (int i = indexForOutput[choiseVictorins - 1]; i < indexForOutput[choiseVictorins]; i++) {
-		for (int j = indexForOutput[choiseVictorins - 1]; j < indexForOutput[choiseVictorins]; j++) {
+
+	for (int i = startQuestion[choiseVictorins - 1]; i < indexForOutput2[choiseVictorins - 1]; i++) {
+		questions[i];
+		for (int j = startVariantes[choiseVictorins - 1]; j < indexForOutput[choiseVictorins]; j++) {
 			variantes[j];
 		}
 		cout << "Enter number variants-> ";
 		cin >> choiseVariants;
 		system("cls");
 		SetColor(15, 0);
-		for (int j = indexForOutput[choiseVictorins - 1]; j < indexForOutput[choiseVictorins]; j++) {
-			if (variantes[choiseVictorins - 1] == trueVariantes[choiseVictorins - 1]) {
-				SetColor(2, 0);
-				variantes[j];
-			}
-			
-			else {
-				SetColor(4, 0);
-				variantes[j];
-			}
-		}
-		SetColor(15, 0);
-		if (variantes[choiseVariants-1] == trueVariantes[i]) {
-			win++;
-		}
+		
 
 	}
-	cout << "Correct answers:" << win << " out of 10" << endl;
+	cout << "Correct answers:" << win << " out of " << indexForOutput2[choiseVictorins] << endl;
 }
 void addNewVictirines() {
 	int question = 0;
 	int variantes2 = 0;
-	int writeStartNumber = 0;
-	victorines + 1;
+	int writeStartNumber = variants;
 	cout << "How many question you want -> ";
 	cin >> question;
 	cout << "How many variants you want -> ";
 	cin >> variantes2;
-	questiones += question;
-	variants += variantes2;
+
 
 	system("cls");
-	indexForOutput[victorines - 1] = variants;
-	indexForOutput2[victorines - 1] = question;
-	string* tempVictorines = new string[1];
-	string* tempQuestion = new string[question];
-	string* tempVariantes = new string[variants*question];
-	string* tempTrueVariantes = new string[question];
-	for (int i = 0; i < victorines+1;i++) {
+
+
+	string* tempVictorines = new string[victorines + 1];
+	string* tempQuestion = new string[questiones + question];
+	string* tempVariantes = new string[(questiones * variants) + (question * variantes2)];
+	int* tempTrueVariantes = new int[questiones + question];
+	for (int i = 0; i < victorines; i++) {
+		tempVictorines[i] = victorine[i];
+	}
+	for (int i = 0; i < questiones; i++) {
+		tempQuestion[i] = questions[i];
+	}
+	for (int i = 0; i < variants; i++) {
+		tempVariantes[i] = variantes[i];
+	}
+	victorines++;
+	indexForOutput = new int[victorines];
+	indexForOutput2 = new int[victorines];
+	startQuestion = new int[victorines];
+	startVariantes = new int[victorines];
+
+	for (int i = 0; i < victorines; i++) {
 		cout << "Enter name your victorines" << endl;
-		getline(cin, tempVictorines[victorines]);
-		for (int j = questiones; j <( question + questiones+1); j++)
+		cin.ignore();
+		getline(cin, tempVictorines[victorines - 1]);
+		for (int j = questiones; j < (question + questiones); j++)
 		{
 			cout << "enter question" << endl;
+			cin.ignore();
 			getline(cin, tempQuestion[j]);
-			
-			for (int g = writeStartNumber; g <( writeStartNumber + variantes2); g++)
+			bool trueVariants = false;
+			for (int g = variants; g < (variants + variantes2); g++)
 			{
-				bool trueVariants = false;
 				cout << "enter variants  " << endl;
+				cin.ignore();
 				getline(cin, tempVariantes[g]);
 				if (trueVariants == false) {
-					cout << "this variants is true";
+					cout << "this variants is true -> ";
 					string ach;
 					cin >> ach;
 					if (ach == "yes") {
-						tempTrueVariantes[j] = tempVariantes[g];
+						tempTrueVariantes[j] = g;
 						trueVariants = true;
 					}
-					else {
-						continue;
-					}
+
 				}
-				
+
 			}
-			writeStartNumber += variantes2;
+
 		}
 	}
-	victorine[victorines] = tempVictorines[0];
-	for (int i = (questiones - question); i < (questiones + question); i++) {
-		questions[i] = tempQuestion[i - questiones];
+	startQuestion[victorines - 1] = questiones;
+	startVariantes[victorines - 1] = variants;
+	questiones += question;
+	variants += variantes2;
+	indexForOutput[victorines - 1] = variants;
+	indexForOutput2[victorines - 1] = questiones;
+
+	victorine = new string[victorines];
+	questions = new string[questiones];
+	variantes = new string[variants];
+	trueVariantes = new int[questiones];
+	for (int i = 0; i < victorines; i++) {
+		victorine[i] = tempVictorines[i];
+	}
+	for (int i = 0; i < questiones; i++) {
+		questions[i] = tempQuestion[i];
 		trueVariantes[i] = tempTrueVariantes[i];
 	}
-	for (int i = (variants - variantes2); i < (variants + variantes2); i++) {
-		variantes[i] = tempVariantes[i - variants];
+	for (int i = 0; i < variants; i++) {
+		variantes[i] = tempVariantes[i];
 	}
-	
 
-	delete[]victorine;
-	delete[]questions;
-	delete[]variantes;
-	delete[]trueVariantes;
+
+	delete[] tempVictorines;
+	delete[] tempQuestion;
+	delete[] tempVariantes;
+	delete[] tempTrueVariantes;
 
 }
 int main() {
@@ -124,9 +141,10 @@ int main() {
 		else if (acshion == 2) {
 			for (int i = 0; i < victorines + 1; i++) {
 				cout << victorine[i] << endl;
+				cout << questions[0];
 				cout << "Choise victorines enter number ->";
 				cin >> choiseVictorins;
-				choiseVictorins--;
+
 				system("cls");
 				output();
 			}
