2f817ad404b9ebb4c6426eaeb8d552fead956483
vadshos <66492574+vadshos@users.noreply.github.com>
2020-10-07 18:36:15 +0300

    Update Source.cpp

--- easy home work/easy home work/Source.cpp
+++ easy home work/easy home work/Source.cpp
@@ -5,24 +5,35 @@ using namespace std;
 
 int main() {
 	//exersise №1
-	/*string str;
-	char arr[255];
-	getline(cin, str);
+	#include <iostream>
+#include <string>
+#include <time.h>
+
+using namespace std;
+int lenghtString(string str) {
+   char arr[255];
+   int index = 0;
+	strcpy_s(arr, str.c_str());
+	for (int i = 0; i < 255; i++) {
+
+		cout << arr[i];
+		if (arr[i] == '\0') {
+			break;
+		}
+		index++;
+}
+	cout << endl;
+	return index;
+}
+
+int main() {
+	int sizeString = 0;
+	string str;
 	int index = 0;
-		strcpy_s(arr, str.c_str());
-		for (int i = 0; i < 255; i++) {
-			
-			cout << arr[i];
-			if (arr[i] == '\0') {
-				break;
-			}
-			index++;
-	}
-		cout << endl;
-		
-		
-		cout << index << endl;
-		return 0;*/
+	getline(cin, str);
+	index = lenghtString(str);
+	cout << "Lenght your string is " << index << endl;
+}
 	//exersise №2
 	string str  ;
 	getline(cin, str);
