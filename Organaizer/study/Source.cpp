f72d2e7ee9812dc2e3e519fb1989670e1ab4bf8d
vadshos <66492574+vadshos@users.noreply.github.com>
2020-10-15 19:45:04 +0300

    Update Source.cpp

--- Organaizer/study/Source.cpp
+++ Organaizer/study/Source.cpp
@@ -1,319 +1,475 @@
-﻿
 #include<iostream>
 #include<string>
+#include<fstream>
+#include<ctime>
+#include <sstream> 
 #include"windows.h"
-#include <stdlib.h>
-
+#include <cstdlib>
+#define Clear system("cls");
 
 using namespace std;
-
-int i = 0;
-const int size1 = 100;
-const int size2 = 3;
-
-int date[size1][size2];
-string events[size1];
-string priority[size1];
-
-float deleted() {
-	int   date1 ;
-	int   date2;
-	int   date3;
-	
-	cout << "enter date what you 	wanted delete" << endl;
-	cin >> date1 >> date2 >> date3;
-	
-	for (int j = 0; j < i; j++) {
-			if (date[j][0] == date1&& date[j][1] == date2&& date[j][2] == date3) {
-				date[j][0] = 0;
-				date[j][1] = 0;
-				date[j][2] = 0;
-				events[j] = "";
-				priority[j] = "";
-
-			}
-			
-	}
-	return 0;
-}
-
-float input() {
-	
-	getline(cin, events[i]);
-	cout << "enter your events -> "<<endl;
-	getline(cin, events[i]);
-	cout << "enter date your events -> " << endl;
-	cin >> date[i][i] >> date[i][i+1] >> date[i][i+2];
-	cout << "enter priority your  events(a,b,c) -> " << endl;
-	getline(cin, priority[i]);
-	getline(cin, priority[i]);
-	i++;
-	return 0;
-}
-float simpleOutput() {
-	string str;
-	for (int j = 0; j < i; j++) {
-		if (priority[j] == "c") {
-			SetColor(4, 0);
-			cout << events[j] << " (" << date[j] << ") " << "importent" << endl;
-			SetColor(15, 0);
-		}else if (priority[j] == "b") {
-			SetColor(14, 0);
-			cout << events[j] << " (" << date[j] << ") " << "midle importent" << endl;
-			SetColor(15, 0);
-		}else if (priority[j] == "a") {
-			SetColor(2, 0);
-			cout << events[j] << " (" << date[j] << ") " << "not a importent" << endl;
-			SetColor(15, 0);
-		}
-		
-		
-	}
-	system("pause");
-	return 0;
-}
-
+int QuentityVariants = 4;
+int QuentityQuizzes = 2;
+int QuentityQuestion = 10;
+bool SingIn = false;
+string login;
+bool bigGame = true;
+bool BigWin = false;
+int win = 0;
 void SetColor(int text, int bg) {
 	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hStdOut, (WORD)((bg << 4) | text));
 }
-enum ConsoleColor {
-	Black = 0,
-	Blue = 1,
-	Green = 2,
-	Cyan = 3,
-	Red = 4,
-	Magenta = 5,
-	Brown = 6,
-	LightGray = 7,
-	DarkGray = 8,
-	LightBlue = 9,
-	LightGreen = 10,
-	LightCyan = 11,
-	LightRed = 12,
-	LightMagenta = 13,
-	Yellow = 14,
-	White = 15
+using namespace std;
+
+struct Options
+{
+	string variants;
+	bool trueVariants;
 };
 
+struct Questions
+{
+	Options* option = new Options[QuentityVariants];
+	string question;
 
-float outputHightDate() {
-	int maxDateYear[size1];
-	int maxDateMounth[size1];
-	int maxDateDays[size1];
-	double maxYear = 0;
-	double maxMounth = 0;
-	double maxDays = 0;
-	bool year = false;
-	for (int j = 0; j < i; j++) {
-		if (date[j][2] > maxYear) {
-			for (int g = 0; g < i; g++) {
-				if (date[j][2] != maxDateYear[g]) {
-					if (g == i-1) {
-						string prio;
-						if (priority[j] == "c") {
-							prio = "importent";
-						}else if (priority[j] == "b") {
-							prio = "midle importent";
-						}
-						else {
-							prio = "not a  importent";
-						}
-						cout << events[j] << " (" << date[j][j] << date[j][j + 1] << date[j][j + 2] << ") " << prio  << endl;
-					}
+};
+struct Quizzes
+{
+	Questions* query = new Questions[QuentityQuestion];
+	string name;
+};
 
-				}
-			}
-		}
-		
-
+void BigGame(string name) {
+	ifstream files;
+	string s = "BigGame";
+	string options;
+	s += name;
+	files.open(s);	
+	s = "";
+	getline(files, s);
+	cout << s;
+	getline(files, s);
+	cout << "correct answer-> ";
+	cin >> options;
+	if (options == s) {
+		BigWin = true;
+		cout << "You win big game my 	greeting";
 	}
-	for (int j = 0; j < i; j++) {
-		if (date[j][1] > maxMounth) {
-			for (int g = 0; g < i; g++) {
-				if (date[j][1] != maxDateMounth[g]) {
-					if (g == i - 1) {
-						string prio;
-						if (priority[j] == "c") {
-							prio = "importent";
-						}
-						else if (priority[j] == "b") {
-							prio = "midle importent";
-						}
-						else {
-							prio = "not a  importent";
-						}
-						cout << events[j] << " (" << date[j][j] << date[j][j + 1] << date[j][j + 2] << ") " << prio << endl;
-					}
-
-				}
-			}
-		}
-
-
-	}
-	for (int j = 0; j < i; j++) {
-		if (date[j][0] > maxDays) {
-			for (int g = 0; g < i; g++) {
-				if (date[j][0] != maxDateDays[g]) {
-					if (g == i - 1) {
-						string prio;
-						if (priority[j] == "c") {
-							prio = "importent";
-						}
-						else if (priority[j] == "b") {
-							prio = "midle importent";
-						}
-						else {
-							prio = "not a  importent";
-						}
-						cout << events[j] << " (" << date[j][j] << date[j][j + 1] << date[j][j + 2] << ") " << prio << endl;
-					}
-
-				}
-			}
-		}
-
-
-	}
-
-	return 0;
+	Sleep(2000);
+	Clear;
 }
-/*float sort_of_ABC() {
-	char a[size1] ;
-	int index1[size1];
-	char tmp;
-	bool f;
-	do
+
+void LogicGame(Quizzes quiz,string name) {
+	int xp = 0;
+	for (int j = 0; j < QuentityQuestion; j++)
 	{
-		f = true;
-		for (int i = 0; i < 9; i++)
-			if (a[i] > a[i + 1])
-			{
-
-				f = false;
-				tmp = a[i];
-				a[i] = a[i + 1];
-				a[i + 1] = tmp;
-
-			}
-	} while (!f);
-	for (int i = 0; i < size1; i++)
-		cout << events[index1[i]] << " (" << date[index1[i]][index1[i]] << date[index1[i]][index1[i+1]] << date[index1[i]][index1[i + 2]] << ") "  << endl;
-	return 0;
-}*/
-
-float output() {
-
-    int b = 0;
-    int a = 0;
-	int c = 0;
-
-
-
-
-	for (int j = 0; j < i; j++) {
-		if (priority[j] == "c") {
-			if (date[j] == 0) {
-				break;
-			}
-			SetColor(4, 0);
-			cout << events[j] << " (" << date[j][j] << date[j][j + 1] << date[j][j + 2] << ") " << "importent" << endl;
-			SetColor(15, 0);
-		}
-	}
-	for (int j = 0; j < i; j++) {
-		if (priority[j] == "b") {
-			if (date[j] == 0) {
-				break;
-			}
-			SetColor(14, 0);
-			cout << events[j] << " (" << date[j][j] << date[j][j + 1] << date[j][j + 2] << ") " << "midle importent" << endl;
-			SetColor(15, 0);
-		}
-	}
-	for (int j = 0; j < i; j++) {
-		if (priority[j] == "a") {
-			if (date[j] == 0) {
-				break;
-			}
-			SetColor(2, 0);
-			cout << events[j] << " (" << date[j][j] << date[j][j + 1] << date[j][j + 2] << ") " << "not a importent" << endl;
-			SetColor(15, 0);
-		}
-
-	}
-	system("pause");
-
-	return 0;
-}
-
-
-void menu() {
-		int action = 0;
-
-		do
+		cout << quiz.query[j].question << endl;
+		for (int i = 0; i < QuentityVariants; i++)
 		{
-			cout << "1.Show all events" << endl;
-			cout << "2.Remove  events" << endl;
-			cout << "3.Add events" << endl;
-			cout << "4.Exit" << endl;
-			cout << endl;
-			cout << "Select action->_";
-			cin >> action;
-
-			switch (action)
-			{
-			case 1: {
-				int ach;
-				cout << "1.output of date add" << endl;
-				cout << "2.output of priority" << endl;
-				cout << "3.output of date" << endl;
-				cin >> ach;
-				if (ach == 1) {
-					simpleOutput();
-					system("cls");
-				}else if (ach == 2) {
-					output();
-					system("cls");
+			cout << quiz.query[j].option[i].variants << endl;			
+		}
+		SetColor(5, 15);
+		cout << "True options " << win << "/10" << endl;
+		SetColor(15, 0);
+		int achion;
+		cout << "Enter option-> ";
+		cin >> achion;
+		int index = 0;
+		bool wins = false;
+		if (quiz.query[j].option[achion - 1].trueVariants == true) {
+			win++;
+			wins = true;
+			index = achion-1;
+		    xp += (100 + rand() % 100)*(j + 1);
+		}
+		else {
+			for (int g = 0; g < QuentityVariants; g++) {
+				if (quiz.query[j].option[g].trueVariants == true) {
+					index = g;
 				}
-				else if (ach == 3) {
-					outputHightDate();
-					system("cls");
-				}
-					
-				
-			}break;
-
-			case 2: {
-				deleted();
-				system("cls");
-				
-			}break;
-
-			case 3: {
-				input();
-			}break;
-
-			case 4: {
-				cout << "Goodbye" << endl;
-			}break;
-
 			}
-			
-		} while (action != 4);
-
+		}
+		Clear;
+		cout << quiz.query[j].question << endl;
+		bool IsTrue = false;					
+				if (wins == false) {
+					for (int i = 0; i < QuentityVariants; i++) {
+						if (i == index) {
+							SetColor(2, 0);
+						}else if(i == achion-1){
+							SetColor(4, 0);
+						}
+						else {
+							SetColor(15, 0);
+						}
+						cout << quiz.query[j].option[i].variants << endl;
+						SetColor(15, 0);
+					}
+				}
+				else {
+					for (int i = 0; i < QuentityVariants; i++)
+					{
+						if (i == index) {
+							SetColor(2, 0);
+						}
+						else {
+							SetColor(15, 0);
+						}
+					cout << quiz.query[j].option[i].variants << endl;
+					SetColor(15, 0);
+					}
+				}
+				Sleep(1500);
+				Clear;
+			SetColor(15, 0);	
 	}
+	ifstream ListAdd;
+	int count = 0;
+	string* arr1 = new string[count+1];
+	ListAdd.open("list.txt", ofstream::app);
+	while (!ListAdd.eof()) {
+		string* temp = new string[count + 1];
+		for (int i = 0; i < count; i++) {
+			temp[i] = arr1[i];
+		}
+		getline(ListAdd, temp[count]);
+		delete[] arr1;
+		count++;
+		arr1 = new string[count];
+		for (int i = 0; i < count; i++) {
+		arr1[i] = temp[i];
+		}
+		temp = nullptr;
+		delete[] temp;
+	}
+	ListAdd.close();
+	SetColor(5, 15);
+	cout<<"True options " << win<<"/10"<<endl;
+	SetColor(15, 0);
+	cout << endl;
+	if (win > 7) {
+		cout << "Do you want to play a big game, win multiply your winnings twice lose lose your winnings.1.yes 2.no " << endl;
+		int  achion = 0;
+		cin >> achion;
+		if (achion == 1) {
+			BigGame(name);
+		}
+	}
+	if (bigGame == true&&BigWin == true) {
+		int index = -1;
+		for (int i = 0; i < (count * 2) + 1; i++) {
+			if (arr1[i] == login) {
+				index = i;
+			}
+		}
+		stringstream geek(arr1[index + 2]);
+		int a = 0;
+		geek << a;
+		xp += a;
+		xp*=2;
+		ofstream List;
+		List.open("list.txt");
+		for (int i = 0; i < count; i++) {
+			if (i == index) {
+				List << arr1[i] << endl;
+				List << xp << endl;
+				i++;
+			}
+			else {
+				List << arr1[i] << endl;
+			}
+		}
+		if (index == -1) {
+			for (int i = 0; i < count; i++) {
+				if (i == count - 1) {
+					List << login << endl;
+					List << xp << endl;
+					i++;
+				}
+				else {
+					List << arr1[i] << endl;
+				}
+			}
+		}
+		List.close();
+	}else if (bigGame == false ) {
+		int index = -1;
+		for (int i = 0; i < (count * 2) + 1; i++) {
+			if (arr1[i] == login) {
+				index = i;
+			}
+		}
+		stringstream geek(arr1[index + 2]);
+		int a = 0;
+		geek << a;
+		xp += a;
+		ofstream List;
+		List.open("list.txt");
+		for (int i = 0; i < count; i++) {
+			if (i == index) {
+				List << arr1[i] << endl;
+				List << xp << endl;
+				i++;
+			}
+			else {
+				List << arr1[i] << endl;
+			}
+		}
+		if (index == -1) {
+			for (int i = 0; i < count; i++) {
+				if (i == count - 1) {
+					List << login << endl;
+					List << xp << endl;
+					i++;
+				}
+				else {
+					List << arr1[i] << endl;
+				}
+			}
+		}
+		List.close();
+	}
+	else if(bigGame == true && BigWin != true){
+		int index = -1;
+		for (int i = 0; i < (count * 2) + 1; i++) {
+			if (arr1[i] == login) {
+				index = i;
+			}
+		}
+
+		
+		xp = 0;
 
 
+		ofstream List;
+		List.open("list.txt");
+		for (int i = 0; i < count; i++) {
+			if (i == index) {
+				List << arr1[i] << endl;
+				List << xp << endl;
+				i++;
+			}
 
+			else {
+				List << arr1[i] << endl;
+			}
+		}
+		if (index == -1) {
+			for (int i = 0; i < count; i++) {
+				if (i == count - 1) {
+					List << login << endl;
+					List << xp << endl;
+					i++;
+				}
 
+				else {
+					List << arr1[i] << endl;
+				}
+			}
+		}
+		List.close();
+	}
+}
+void randomQuiz() {
+	ifstream files;
+	string name;
+	files.open("random.txt");
+	int count = 0;
+	string* arr1 = new string[count];
+	while (!files.eof()) {
+		string* temp = new string[count + 1];
+		for (int i = 0; i < count; i++) {
+			temp[i] = arr1[i];
+		}
+		getline(files, temp[count]);
+		delete[] arr1;
+		count++;
+		arr1 = new string[count];
+		arr1 = temp;
+		temp = nullptr;
+		delete[] temp;
+	}
+	int index = rand() % count + 1;
+	name = arr1[index];
+	string txt = ".txt";
+	name += txt;
+	files.close();
+	ifstream file;
+	file.open(name);
 
+	Quizzes quiz;
+	for (int i = 0; i < QuentityQuestion; i++) {
+		getline(file, quiz.query[i].question);
+		for (int j = 0; j < QuentityVariants; j++) {
+			getline(file, quiz.query[i].option[j].variants);
+			if (j == QuentityVariants - 1) {
+				string TrueVar;
+				getline(file, TrueVar);
+				for (int g = 0; g < QuentityVariants; g++) {
+					if (quiz.query[i].option[g].variants == TrueVar) {
+						quiz.query[i].option[g].trueVariants = true;
+					}
+					else {
+						quiz.query[i].option[g].trueVariants = false;
+					}
+				}
+			}
 
-
-
+		}
+	}
+	LogicGame(quiz,name);
+}
+void outputQuizzer(int a) {
+	ifstream files;
+	string name;
+	int achion = 0;
+	if (a == 1) {
+		files.open("nameQuizzerProgramming.txt");
+	}
+	else if (a == 2) {
+		files.open("nameQuizzerAdministration.txt");
+	}
+	else if (a == 3) {
+		files.open("nameQuizzerLife.txt");
+	}
+	
+	
+	int count = 0;
+	string* arr1 = new string[count];
+	while (!files.eof()) {
+		string* temp = new string[count + 1];
+		for (int i = 0; i < count; i++) {
+			temp[i] = arr1[i];
+		}
+		getline(files, temp[count]);
+		delete[] arr1;
+		count++;
+		arr1 = new string[count];
+		arr1 = temp;
+		temp = nullptr;
+		delete[] temp;
+	}
+	for (int i = 0; i < count; i++) {
+		cout<<"" << i + 1 << ". " << arr1[i] << endl;
+	}
+	cout << "Enter number quizeer-> ";
+	cin >> achion;
+	Clear;
+	system("mode con cols=80 lines=10");
+	string txt = ".txt";
+	name = arr1[achion - 1];
+	name += txt;
+	files.close();
+	files.open(name);
+	count = 0;
+	string* arr2 = new string[count];
+	Quizzes quiz;
+	for (int i = 0; i < QuentityQuestion; i++) {
+		getline(files, quiz.query[i].question);
+		for (int j = 0; j < QuentityVariants;j++) {
+			getline(files, quiz.query[i].option[j].variants);
+			if (j == QuentityVariants - 1) {
+				string TrueVar;
+				getline(files, TrueVar);
+				for (int g = 0; g < QuentityVariants; g++) {
+					if (quiz.query[i].option[g].variants == TrueVar) {
+						quiz.query[i].option[g].trueVariants = true;
+					}
+					else {
+						quiz.query[i].option[g].trueVariants = false;
+					}
+				}
+			}
+		  
+		}
+	}
+	LogicGame(quiz,name);
+}
+void List() {
+	int achion = 0;
+	while (achion != 6) {
+		cout << "List group quiz" << endl;
+		cout << "1.Programming" << endl;
+		cout << "2.Administration" << endl;
+		cout << "3.Life quiz" << endl;
+		cout << "4.Random quiz" << endl;
+		cout << "5.List statik users" << endl;
+		cout << "6.EXIT" << endl;
+		cout << "Enter achion-> ";		
+		cin >> achion;
+		Clear;
+		if (achion > 0 && achion < 4) {
+			outputQuizzer(achion);
+		}
+		else if (achion == 4) {
+			randomQuiz();
+		}
+		else if (achion == 5) {
+			ifstream List;
+			List.open("list.txt");
+			while (!List.eof()) {
+				string log = "";
+				string xp = "";
+				getline(List, log);
+				getline(List, xp);
+				if (log != "" && xp != "") {
+				cout << log << " - " << xp << endl;
+				}
+			}
+		}
+	}	
+}
+void Login() {
+	system("mode con cols=25 lines=10");
+	string password;
+	cout << "\tLOGIN" << endl;
+	cout << "\t ";
+	cin >> login;
+	cout << "\tPASSWORD" << endl;	
+	cout << "\t";
+	cin >> password;
+	ifstream singIn;
+	singIn.open("D:\\Study IT STEP academy\\Новая папка\\ConsoleApplication1\\ConsoleApplication1\\users\\login.txt");
+	Clear;
+	
+	while (!singIn.eof()) {
+		bool isTrueLogin = false;
+		bool isTruePassword = false;
+		string l;
+		string p;
+		getline(singIn,l);
+		getline(singIn, p);
+		if (l == login) {
+			isTrueLogin = true;
+		}
+		if (p == password) {
+			isTruePassword = true;
+		}
+		if (isTruePassword == true && isTrueLogin == true) {
+			SingIn = true;
+		}		
+		List();
+	}
+	singIn.close();
+	if (SingIn == false) {
+		ofstream SingAdd;
+		SingAdd.open("D:\\Study IT STEP academy\\Новая папка\\ConsoleApplication1\\ConsoleApplication1\\users\\login.txt", ofstream::app);
+		SingAdd << endl;
+		SingAdd <<login<<endl;
+		SingAdd << password;
+		SingAdd.close();
+		string str2 = "D:\\Study IT STEP academy\\Новая папка\\ConsoleApplication1\\ConsoleApplication1\\users\\"+login+".txt";
+		SingAdd.open(str2, ofstream::app);
+		SingAdd.close();
+	}
+	
+	Clear;
+}
 
 int main() {
-	
-	menu();
+	srand(time(NULL));
+	Login();
+
+	return 0;
 }
