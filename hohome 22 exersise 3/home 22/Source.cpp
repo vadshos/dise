ce7b0354a5ffe741281f34c4a04e127d4be62664
vadshos <66492574+vadshos@users.noreply.github.com>
2020-12-15 16:56:52 +0200

    Update Source.cpp

--- hohome 22 exersise 3/home 22/Source.cpp
+++ hohome 22 exersise 3/home 22/Source.cpp
@@ -1,117 +1,226 @@
-#include<iostream>
-#include<string>
-using namespace std;
+#pragma once
+template<typename MyType>
+class Array
+{
+public:
+	Array(MyType arr,size_t size);
+	Array(const Array& obj);
+	const size_t& GetSize() const;
+	Array& operator = (Array& obj);
+	const size_t& GetUpperBound() const;
+	void FreeExtra() const;
+	void SetSize(int size, int grow = 1);
+	const Array& InsertAt( size_t index,MyType element);
+	MyType & operator[](size_t index);
+	const Array Append(const Array& obj);
+	const Array& RemoveAt(const size_t index);
+	const MyType& GetAt(size_t index) ;
+	Array<MyType>& SetAt(size_t index, MyType element);
+	bool isEmpty() const;
+	const Array& RemoveAll();
+    Array*  GetData();
+	template <typename T> friend std::ostream& operator<<(std::ostream& out, const Array<T>& a);
+	~Array();
 
-const int sizeGroup = 20;
-string group[sizeGroup];
-int currentIndex = 0;
+private:
+	MyType* arr = nullptr;
+	size_t size = 0;
+	size_t grow = 0;
+	size_t capacity = 0;
+};
 
-
-
-
-
-void showAllStudents() {
-	bool isEmpty = true;
-	for (int i = 0; i < sizeGroup; i++)
+template<typename MyType>
+inline Array<MyType>::Array(MyType arr, size_t size) 
+{
+	this->size = size  < 1 ? 1 : size;
+	this->capacity = this->size;
+	this->grow = 0;
+	this->arr = new MyType[this->size];
+	for (size_t i = 0; i < this->size; i++)
 	{
-		if (group[i] != "") {
-			cout << i + 1 << "." << group[i] << endl;
-			isEmpty = false;
-		}
+		this->arr[i] = arr;
 	}
-
-	if (isEmpty == true) {
-		cout << "List is empty!" << endl;
-	}
-	cout << endl;
-
-}
-template <typename Type1>
-Type1 addStudent(Type1 nameOfStudent) {
-	if (currentIndex < sizeGroup) {
-		group[currentIndex] = nameOfStudent;
-		currentIndex++;
-	}
-	else {
-		cout << "Group is full" << endl;
-	}
-	cout << endl;
-
-}
-
-
-template <typename Type>
-Type removeStudent(Type indexOfStudent) { //1
-	group[indexOfStudent] = "";
-
-	string tempArray[sizeGroup];
-	int tempIndex = 0;
-
-	for (string item : group)
-	{
-		if (item != "") {
-			tempArray[tempIndex] = item;
-			tempIndex++;
-		}
-	}
-
-	for (int i = 0; i < sizeGroup; i++)
-	{
-		group[i] = tempArray[i];
-	}
-	cout << endl;
 }
 
-
-void managerStudents() {
-	int action = 0;
-
-	do
+template<typename MyType>
+inline Array<MyType>::Array(const Array& obj)
+{
+	if (this != &obj)
 	{
-		cout << "1.Show all students" << endl;
-		cout << "2.Remove student" << endl;
-		cout << "3.Add student" << endl;
-		cout << "4.Exit" << endl;
-		cout << endl;
-		cout << "Select action->_";
-		cin >> action;
-
-		switch (action)
+		delete[] this->arr;
+		size = obj.size;
+		grow = obj.grow;
+		capacity = obj.capacity;
+		this->arr = new MyType[size];
+		for (size_t i = 0; i < size; i++)
 		{
-		case 1: {
-			system("cls");
-			showAllStudents();
-		}break;
-
-		case 2: {
-			system("cls");
-			int tempNumber = 0;
-			cout << "Enter number student for delete: ";
-			cin >> tempNumber;
-			removeStudent(tempNumber - 1);
-		}break;
-
-		case 3: {
-			system("cls");
-			string temp;
-			cout << "Enter name for new student: ";
-			cin >> temp;
-
-			addStudent(temp);
-		}break;
-
-		case 4: {
-			cout << "Goodbye" << endl;
-		}break;
-
+			arr[i] = obj.arr[i];
 		}
-	} while (action != 4);
+	}
+}
+
+template<typename mytype>
+inline const size_t& Array<mytype>::GetSize() const
+{
+	return capacity;
+}
+
+template<typename MyType>
+inline Array<MyType>& Array<MyType>::operator=(Array& obj)
+{
+	if (this != &obj)
+	{
+		delete[] this->arr;
+		size = obj.size;
+		grow = obj.grow;
+		capacity = obj.capacity;
+		this->arr = new MyType[size];
+		for (size_t i = 0; i < size; i++)
+		{
+			arr[i] = obj.arr[i];
+		}
+	}
+	return *this;
+}
+
+template<typename MyType>
+inline const size_t& Array<MyType>::GetUpperBound() const
+{
+	return size;
+}
+
+template<typename MyType>
+inline void Array<MyType>::FreeExtra() const
+{
+	capacity = size;
+}
+
+template<typename MyType>
+inline void Array<MyType>::SetSize(int size, int grow)
+{
+	this->size = size;
+	this->grow = grow;
 
 }
 
-int main() {
 
-	managerStudents();
 
-	return 0;
+template<typename MyType>
+inline const Array<MyType>& Array<MyType>::InsertAt( size_t index,MyType element)
+{
+	MyType* temp = new MyType[size];
+	for (size_t i = 0; i < size; i++)
+	{
+		temp[i] = arr[i];
+	}
+	delete[] arr;
+	arr = new MyType[size+1];
+	for (size_t i = 0,j =0; i < size+1; i++)
+	{
+		i != index - 1 ? arr[i] = temp[j], j++ : arr[i] = element;
+	}
+	size++;
+	if (capacity < size) {
+		grow == 0 ? capacity ++ : capacity += grow;
+	}
+	return *this;
+}
+
+template<typename MyType>
+inline MyType& Array<MyType>::operator[](size_t index)
+{
+	return arr[index];
+}
+
+template<typename MyType>
+inline const Array<MyType> Array<MyType>::Append(const Array& obj)
+{
+	size_t size_s = size + obj.size;
+	Array<MyType> tempArr(size_s+1,MyType());
+	for (int i = 0; i < size; i++) {
+		tempArr.SetAt(i,arr[i]);
+	}
+	for (int i = size , j = 0; i < size_s; i++) {
+		tempArr.SetAt(i, obj.arr[i]);
+	}
+	return tempArr;
+}
+
+
+
+template<typename MyType>
+inline const Array<MyType>& Array<MyType>::RemoveAt(const size_t index)
+{
+	MyType* temp = new MyType[size-1];
+	for (size_t i = 0,j=0; i < size; i++)
+	{
+		if (i != index) {
+			temp[j] = arr[i];
+			j++;
+		}
+	}
+	delete[] arr;
+	arr = nullptr;
+	size = size - 1;
+	arr = new MyType[size];
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = temp[i];
+	}
+	return *this;
+}
+
+template<typename MyType>
+inline const MyType& Array<MyType>::GetAt(size_t index)
+{
+	return arr[index] ;
+}
+
+template<typename MyType>
+inline  Array<MyType>& Array<MyType>::SetAt(size_t index, MyType element)
+{
+	if (size >= index) {
+		arr[index] = element;
+	}
+	return *this;
+}
+
+template<typename MyType>
+inline bool Array<MyType>::isEmpty() const
+{
+	return arr == nullptr ? true : false;
+}
+
+template<typename MyType>
+inline const Array<MyType>& Array<MyType>::RemoveAll()
+{
+	delete[] arr;
+	arr = nullptr;
+	size = 0;
+	return *this;
+}
+
+template<typename MyType>
+inline  Array<MyType>* Array<MyType>::GetData()  
+{
+	return this;
+}
+
+template<typename MyType>
+inline Array<MyType>::~Array()
+{
+	if (arr != nullptr) {
+		RemoveAll();
+	}
+}
+
+template<typename T>
+inline std::ostream& operator<<(std::ostream& out, const Array<T>& a)
+{
+	for (size_t i = 0; i < a.size; i++)
+	{
+
+		out << a.arr[i]<<" ";
+	}
+	return out;
 }
