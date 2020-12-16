6e9d26e60649044ee17bb2c318e9cc718cb5045d
vadshos <66492574+vadshos@users.noreply.github.com>
2020-12-16 23:05:59 +0200

    Update Source.cpp

--- easy home work/easy home work/Source.cpp
+++ easy home work/easy home work/Source.cpp
@@ -1,132 +1,305 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-using namespace std;
+pragma once
+#include <iostream>
 
-
-int main() {
-	cin.ignore();
-	int i = 3;
-	int index = 0;
-	string login;
-	string password;
-	bool FirstLogin = true;
-	bool FailSingIn = false;
-	int count = 0;
-	string* arr1 = new string[count];
-	ifstream ckeckLogin;
-
-	ckeckLogin.open("Login.txt");
-	string secretWord;
-	cout << "Login -> ";
-	cin >> login;
-	cout << "Password -> ";
-	cin >> password;
-	while (!ckeckLogin.eof()) {
-		string l;
-		string p;
-		getline(ckeckLogin, l);
-		getline(ckeckLogin, p);
-		getline(ckeckLogin, secretWord);
-		
-		if (login == l) {
-			FirstLogin = false;			
-			if(p != password) {
-				FailSingIn = true;
-				break;
-			}
-		}
-		else if (login + "*" == l) {
-			FailSingIn = true;
-			cout << "You account blocked" << endl;
-			break;
-		}
-		index += 3;
-	}
-	ckeckLogin.close();
-	if (FailSingIn == true){
-		int achion = 0;
-		cout << "You enter incorect password you want reset password 1.yes 2.no";
-		cin >> achion;
-		if (achion == 1) {
+template<typename T>
+class Dlist
+{
+public:
+	Dlist() = default;
+	Dlist(const Dlist& obj);
+	void pushFront(const T& data);
+	void pushTail(const T& data);
+	void popFront();
+	void popBack();
+	void pushAfter(const T& searchData, const T& data);
+	void pushBefore(const T& searchData, const T& data);
+	auto findNode(const T& data);
+	bool replace(const T& SearchData, const T& data);
+	void reverse();
+	void popSomeElement(const T& data);
+	bool isEmpty() const;
+	Dlist concat(Dlist obj);
+	Dlist interseption(Dlist obj);
+	size_t getSize() const;
+	void print();
+	void clear();
+	void rPrint();
+private:
+	struct Node
+	{
+		T data;
+		Node* next,* prev;
+		Node(const T& data = T(), Node* prev = nullptr, Node* next = nullptr)
+			:data(data), next(next), prev(prev)
+		{
 			
-			while (i != 0) {
-				string s;
-				cin.ignore();
-				cout << "Enter secret word you have "<<i<<" try";
-				getline(cin, s);
-				if (s == secretWord) {
-					ifstream editPassword;
-					editPassword.open("Login.txt");
-					
-					while (!editPassword.eof()) {
-						string* temp = new string[count + 1];
-						for (int i = 0; i < count; i++) {
-							temp[i] = arr1[i];
-						}
-						getline(editPassword, temp[count]);
-						delete[] arr1;
-						count++;
-						arr1 = new string[count];
-						arr1 = temp;
-						temp = nullptr;
-						delete[] temp;
-					}
-					editPassword.close();
-					ofstream file("Login.txt");
-					for (int i = 0; i < count; i++) {
-						if (i != index + 2) {
+		};
+	};
+	
+	/*~Dlist();*/
+	Node* head = nullptr;
+	Node* tail = nullptr;
+	size_t size = 0;
+};
 
-							file << arr1[i] << endl;
-						}
-						else {
-							file << password << endl;
-							cout << "You new password is " << password << endl;
-						}
-					}
-					break;
-				}
-				else {
-					i--;
-				}
-			}
+template<typename T>
+inline Dlist<T>::Dlist(const Dlist& obj)
+{
+	Node* temp = obj.head;
+	while (temp != nullptr) {
+		pushTail(temp->data);
+		temp = temp->next;
+	}
+}
+
+template<typename T>
+inline void Dlist<T>::pushFront(const T& data)
+{
+	Node* tmp = new Node(data,nullptr,head);
+	if (isEmpty())
+	{
+		tail = tmp;
+	}
+	else
+	{
+		head->prev = tmp;
+	}
+	++size;
+	head = tmp;
+
+}
+
+template<typename T>
+inline void Dlist<T>::pushTail(const T& data)
+{
+	Node* add = new Node(data,tail,nullptr);
+	if (!isEmpty())
+	{
+		tail->next = add;
+	}
+	else {
+		head = add;
+	}
+	size++;
+	tail = add;
+}
+
+template<typename T>
+inline void Dlist<T>::popFront()
+{
+	if (!isEmpty())
+	{
+		auto tmp = head;
+		head = head->next;
+		if (isEmpty())
+		{
+			tail = nullptr;
 			
 		}
 		else {
-			cout << "Goodbye" << endl;
+			head->prev = nullptr;
+		}
+		delete tmp;
+		--size;
+	}
+	
+}
+
+template<typename T>
+inline void Dlist<T>::popBack()
+{
+	if (!isEmpty())
+	{
+		if(size == 1){
+			clear();
+		}
+		else {
+			Node* temp = tail->prev;
+			delete tail;
+			tail = temp;
+			temp->next = nullptr;
 		}
 	}
-	if (FirstLogin == true && FailSingIn == false) {
-		ofstream addLogin;
-		cout << "We create you new accout because you new user in system"<<endl;
-		cout << "Please enter secret word for you account" << endl;
-		getline(cin, secretWord);
-		addLogin.open("Login.txt",ofstream::app);
-		addLogin << login<<endl;
-		addLogin << password << endl;
-		cout << "Please enter secret word for you account" << endl;
-		getline(cin, secretWord);
-		addLogin << secretWord << endl;
-		cout << "Your login is " << login << endl;
-		cout << "Your password is " << password << endl;
-		cout << "Please don't forget your login and password" << endl;
-		addLogin.close();
+	else {
+		head = nullptr;
+		tail = nullptr;
 	}
-	else if(FailSingIn == false) {
-		cout << "Sing in successful" << endl;
-	}
-	if (i == 0) {
-		ofstream banLogin("Login.txt");
-		for (int i = 0; i < count; i++) {
-			if (i != index) {
+}
 
-				banLogin << arr1[i] << endl;
-			}
-			else {
-				banLogin << login+"*" << endl;
-			}
+template<typename T>
+inline void Dlist<T>::pushAfter(const T& searchData, const T& data)
+{
+	Node* find = findNode(searchData);
+	if (find != nullptr)
+	{
+		Node* ins = new Node(data,  find->prev ,find->next);
+		find->next = ins;
+		if (find == tail)
+		{
+			tail = ins;
 		}
 	}
+}
 
-	return 0;
+template<typename T>
+inline void Dlist<T>::pushBefore(const T& searchData, const T& data)
+{
+	Node* find = findNode(searchData);
+	if (find != nullptr)
+	{
+		Node* ins = new Node(data, find->prev, find);
+		find->prev = ins;
+		find->prev->prev->next = ins;
+
+		if (find == tail)
+		{
+			tail = ins;
+		}
+	}
+}
+
+template<typename T>
+inline auto Dlist<T>::findNode(const T& data)
+{
+	Node* tmp = head;
+	while (tmp != nullptr && tmp->data != data)
+	{
+		tmp = tmp->next;
+	}
+	return tmp;
+}
+
+template<typename T>
+inline bool Dlist<T>::replace(const T& SearchData, const T& data)
+{
+	Node* find = findNode(SearchData);
+	if (find != nullptr)
+	{
+		find->data = data;
+		if (find == tail)
+		{
+			tail->data = data;
+		}
+		return true;
+	}
+	return false;
+}
+
+template<typename T>
+inline void Dlist<T>::reverse()
+{
+	Node* temp = head;
+	Dlist <T> reversed_list;
+	while (temp != nullptr) {
+		reversed_list.pushFront(temp->data);
+		temp = temp->next;
+	}
+	this->clear();
+	*this = reversed_list;
+}
+
+
+template<typename T>
+inline void Dlist<T>::popSomeElement(const T& data)
+{
+	Node* find = findNode(data);
+	if (find != nullptr)
+	{
+		if (find == head) {
+			popFront();
+		}
+		else if(find == tail) {
+			popBack();
+		}
+		else if (find->next != nullptr)
+		{
+			find->prev->next = find->next;
+			find->next->prev = find->prev;
+			delete find;
+		}
+		--size;
+
+
+	}
+}
+
+template<typename T>
+inline bool Dlist<T>::isEmpty() const
+{
+	return head == nullptr;
+}
+
+template<typename T>
+inline Dlist<T> Dlist<T>::concat(Dlist obj)
+{
+	Dlist<T>  c;
+	Node* temp = head;
+	while (temp != tail) {
+		c.pushFront(temp->data);
+		temp = temp->next;
+	}
+	temp = obj.head;
+	while (temp != obj.tail) {
+		c.pushFront(temp->data);
+		temp = temp->next;
+	}
+	return c;
+}
+
+template<typename T>
+inline Dlist<T> Dlist<T>::interseption(Dlist obj)
+{
+	Dlist<T>  c;
+	Node* temp = head;
+	Node* tmp = obj.head;
+	while (temp != tail) {
+		while (tmp != obj.tail) {
+			if (temp->data == tmp->data) {
+				c.pushTail(temp->data);
+				tmp = tmp->next;
+				break;
+			}
+		}
+		temp = temp->next;
+	}
+	return c;
+}
+
+template<typename T>
+inline size_t Dlist<T>::getSize() const
+{
+	return size;
+}
+
+template<typename T>
+inline void Dlist<T>::print()
+{
+	Node* temp = head;
+	while (temp != nullptr)
+	{
+		std::cout<< temp->data<<std::endl;
+		temp = temp->next;
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+inline void Dlist<T>::clear()
+{
+	while (!isEmpty())
+	{
+		popFront();
+	}
+}
+
+template<typename T>
+inline void Dlist<T>::rPrint()
+{
+	Node* temp = tail;
+	while (temp != head)
+	{
+		std::cout << temp->data << std::endl;
+		temp = temp->prev;
+	}
+	std::cout << std::endl;
 }
