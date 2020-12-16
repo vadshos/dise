1b7c58eb4ee546bbe8ac940237da46d8d1c9f065
vadshos <66492574+vadshos@users.noreply.github.com>
2020-12-16 10:10:34 +0200

    Update Source.cpp

--- homework/homework/Source.cpp
+++ homework/homework/Source.cpp
@@ -1,215 +1,256 @@
-#include<iostream>
-#include<string>
-#include<iomanip>
-#include<conio.h>
-#include"windows.h"
-
+#pragma once
+//#define MY_DEBUG
+#include <iostream>
 using namespace std;
-
-void SetColor(int text, int bg) {
-    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hStdOut, (WORD)((bg << 4) | text));
-}
-
-class Student
+template<typename T>
+class FwdList
 {
-  public:
-      class Date
-      {      
-      public:
-          static const size_t CURR_YEAR = 2020;
-          void setYear(const size_t& year_) {
-              if(year>= CURR_YEAR - 16)
-              year = year_;            
-          }
-          void setMonth(const size_t& month_) {
-              if (month_ <= 12 && month_ >= 1) {
-                  month = month_;
-              }else {
-                  cout << "You enter inccrect  data" << endl;
-                  cout << "Enter date";
-                  int temp;
-                  cin >> temp;
-                  setMonth(temp);
+public:
+	FwdList() = default;
+	FwdList(const FwdList& obj);
+	FwdList& operator = (const FwdList& obj);
+	bool operator == (const FwdList& obj);
+	void pushFront(const T& data);
+	void print()const;
+	void eraseFront();
+	void pushAfter(const T& searchData, const T& data);
+	bool Swap(const T& searchData, const T& data);
+	void move();
+	void clear();
+	void eraseAfter(const T& searchData);
+	void eraseTail();
+	void addTail(const T& data);
 
-              }
 
-          }
-          void setDay(const size_t & day_) {
-                  size_t DaysInMonth[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
-                  if (day_ <= DaysInMonth[month]) {                    
-                      day = day_;
-                  } else if (year % 4 != 0 && year % 100 == 0 || year % 400 != 0) {
-                          if (day_ <= 29 && month == 2) {
-                              day = day_;
-                          }
-                          else if (day_ <= DaysInMonth[month]) {
-                              day = day_;
-                          }
-                  }else {
-                      
-                      cout << "You enter inccrect  data" << endl;
-                      cout << "Enter date";
-                     int temp;
-                     cin >> temp;
-                      setDay(temp);
-          
-                  }        
-          }
-          const size_t & getDay() {
-              return day;
-          }
-          const size_t & getMonth() {
-              return month;
-          }
-          const size_t & getYear() {
-              return year;
-          }
-      private:
-          size_t day, month, year;
-      };
-      void setNumber(string& number_) {
-          size_t intNumber  = 0;
-          bool  corectnumber = true;
-          try {
-              intNumber = stoi(number_);
-          }
-          catch (std::invalid_argument& e) {
-              corectnumber = false;
-          }
-          if (corectnumber == true && size(number_)) {
-              number = number_;
-          }
-          else {
-              cout << "Enter  correct number ";
-              string temp;
-              getline(cin, temp);
-              setNumber(temp);
-          }
-      }
-      void setName(const string& Name_) {
-          if (!Name_.empty()) {
-              name = Name_;
-          }
-          else {
-              cout << "You enter inccrect  data" << endl;
-              cout << "Enter date";
-              string temp;
-              getline(cin, temp);
-              setName(temp);
-          }
-      }
-      void setLastname(const string& Lastname_) {
-          if (!Lastname_.empty()) {
-              Lastname = Lastname_;
-          }
-          else {
-              cout << "You enter inccrect  data" << endl;
-              cout << "Enter date";
-              string temp;
-              getline(cin, temp);
-              setLastname(temp);
-          }
-      }
-      void setFather(const string& Father_) {
-          if (!Father_.empty()) {
-              Lastname = Father_;
-          }
-          else {
-              cout << "You enter inccrect  data" << endl;
-              cout << "Enter date";
-              string temp;
-              getline(cin, temp);
-              setFather(temp);
-          }
-      }
-      void setCity(const string& city_) {
-          if (!city_.empty()) {
-              city = city_;
-          }else {
-              cout << "You enter inccrect  data" << endl;
-              cout << "Enter date";
-              string temp;
-              getline(cin, temp);
-              setCity(temp);
-          }
-      }
-      void setCountry(const string& country_) {
-          if (!country_.empty()) {
-              country = country_;
-          } else {
-              cout << "You enter inccrect  data" << endl;
-              cout << "Enter date";
-              string temp;
-              getline(cin, temp);
-              setCountry(temp);
-          }
-      }
-      void setNameUniversity(const string& NameUniversity_) {
-          if (!NameUniversity_.empty()) {
-              NameUniversity = NameUniversity_;
-          }else {
-              cout << "You enter inccrect  data" << endl;
-              cout << "Enter date";
-              string temp;
-              getline(cin, temp);
-              setNameUniversity(temp);
-          }
-      }
-      void setNumberGroup(const string& NumberGroup_) {
-          if (!NumberGroup_.empty()) {
-              NumberGroup = NumberGroup_;
-          }
-      }
-      void setBirth(const Date& birth_) {
-          birth = birth_;
-      }
-      const string& getName() {
-                  return name;
-             }
-      const string & getLastname() {
-                  return Lastname;
-      }
-      size_t * getBirth() {
-         
-         size_t day_ = birth.getDay();
-         size_t month_ = birth.getMonth();
-         size_t year_ = birth.getYear();
-         static size_t arr[3] = { day_ , month_ , year_ };
-          return  arr;
-      }
-      const string& getNumberGroup() {
-          return NumberGroup;
-      }
-      const string& getNameUniversity() {
-          return NameUniversity;
-      }
-      const string& getCountry() {
-          return country;
-      }
-      const string& getCity() {
-          return city;
-      }
-      const string& getNumber() {
-          return number;
-      }
-      const string& getFather() {
-          return Father;
-      }
-      Date birth;
-private:  
-      string name;
-      string Lastname;
-      string Father;
-      string number;
-      string city;
-      string country;
-      string NameUniversity;
-      string NumberGroup;
-     
-
+	~FwdList();
+private:
+	auto findNode(const T& data);
+	struct Node {
+		T data;
+		Node* next;
+		Node(const T& data, Node* next = nullptr)
+			:data(data), next(next)
+		{}
+	};
+	Node* head = nullptr;
+	Node* tail = nullptr;
+	const bool isEmpty()const;
 };
 
-int main() {
-    
+template<typename T>
+inline FwdList<T>::FwdList(const FwdList& obj)
+{
+	Node* temp = obj.head;
+	while (temp != nullptr) {
+		addTail(temp->data);
+		temp = temp->next;
+	}
+}
+
+template<typename T>
+inline FwdList<T>& FwdList<T>::operator=(const FwdList& obj)
+{
+	Node* temp = obj.head;
+	while (temp != nullptr) {
+		addTail(temp->data);
+		temp = temp->next;
+	}
+	return *this;
+}
+
+template<typename T>
+inline bool FwdList<T>::operator==(const FwdList& obj)
+{
+	Node* tempFirst = head;
+	Node* tempSecond = obj.head;
+
+		while (tempFirst != tail && tempSecond != obj.tail) {
+			if (tempFirst->data == tempSecond->data) {
+				tempFirst = tempFirst->next;
+				tempSecond = tempSecond->next;
+
+			}
+			else {
+				return false;
+			}
+		}
+	
+		if (tempFirst != nullptr && tempSecond != nullptr) {
+			if (tempFirst->data == tempSecond->data) {
+				return true;
+			}
+		}
+			return false;
+}
+
+template<typename T>
+inline void FwdList<T>::pushFront(const T& data)
+{
+	Node* tmp = new Node(data, head);
+	if (isEmpty())
+	{
+		tail = tmp;
+	}
+	head = tmp;
+
+}
+
+template<typename T>
+inline void FwdList<T>::print() const
+{
+	Node* tmp = head;
+	while (tmp != nullptr)
+	{
+		cout << tmp->data << " ";
+		tmp = tmp->next;
+	}
+	cout << endl;
+}
+
+template<typename T>
+inline void FwdList<T>::eraseFront()
+{
+	if (!isEmpty())
+	{
+		auto tmp = head;
+		head = head->next;
+		if (isEmpty())
+		{
+			tail = nullptr;
+		}
+#ifdef MY_DEBUG
+		cout << "Was deleted " << tmp->data << endl;
+#endif 
+		delete tmp;
+	}
+}
+
+template<typename T>
+inline void FwdList<T>::pushAfter(const T& searchData, const T& data)
+{
+	Node* find = findNode(searchData);
+	if (find != nullptr)
+	{
+		Node* ins = new Node(data, find->next);
+		find->next = ins;
+		if (find == tail)
+		{
+			tail = ins;
+		}
+	}
+}
+
+template<typename T>
+inline bool FwdList<T>::Swap(const T& searchData, const T& data)
+{
+	Node* find = findNode(searchData);
+	if (find != nullptr)
+	{
+		find->data = data;
+
+		if (find == tail)
+		{
+			tail->data = data;
+		}
+		return true;
+	}
+	return false;
+}
+
+template<typename T>
+inline void FwdList<T>::move()
+{
+
+	Node* temp = head;
+	FwdList <T> reversed_list;
+	while (temp != nullptr) {
+		reversed_list.pushFront(temp->data);
+		temp = temp->next;
+	}
+	this->clear();
+	*this = reversed_list;
+}
+
+
+
+template<typename T>
+inline void FwdList<T>::clear()
+{
+	while (!isEmpty())
+	{
+		eraseFront();
+	}
+}
+
+template<typename T>
+inline void FwdList<T>::eraseAfter(const T& searchData)
+{
+	Node* find = findNode(searchData);
+	if (find != nullptr)
+	{
+		if (find->next != nullptr)
+		{
+			auto tmp = find->next;
+			find->next = tmp->next;
+			delete tmp;
+		}
+
+
+
+	}
+}
+
+template<typename T>
+inline void FwdList<T>::eraseTail()
+{
+
+	if (!isEmpty()) {
+		Node* temp = head;
+		while (temp->next != tail) {
+			temp = temp->next;
+		}
+
+		delete tail;
+		tail = temp;
+		temp->next = nullptr;
+	}
+}
+
+template<typename T>
+inline void FwdList<T>::addTail(const T& data)
+{
+	Node* add = new Node(data);
+	if (!isEmpty())
+	{
+		tail->next = add;
+	}
+	else {
+		head = add;
+	}
+	tail = add;
+}
+
+template<typename T>
+inline FwdList<T>::~FwdList()
+{
+	clear();
+}
+
+template<typename T>
+inline auto FwdList<T>::findNode(const T& data)
+{
+	Node* tmp = head;
+	while (tmp != nullptr && tmp->data != data)
+	{
+		tmp = tmp->next;
+	}
+	return tmp;
+}
+
+
+
+template<typename T>
+inline const bool FwdList<T>::isEmpty() const
+{
+	return head == nullptr;
 }
